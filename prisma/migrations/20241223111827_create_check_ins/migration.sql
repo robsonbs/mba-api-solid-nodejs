88770dcc93e87bf6db396c7f3d0bc1a2ae868308
robson.souza <robson.souza@presidencia.gov.br>
2024-12-23 19:05:56 -0300

    chore: database migrations

---
+++ prisma/migrations/20241223111827_create_check_ins/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "validated_at" TIMESTAMP(3),
+
+    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+);
