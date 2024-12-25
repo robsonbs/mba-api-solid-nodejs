217d68971cd2701f3d03fcabb66c90e837265edb
robson.souza <robson.souza@presidencia.gov.br>
2024-12-25 20:33:48 -0300

    build: install `@fastify/jwt`

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  JWT_SECRET: z.string(),
   // DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('pg'),
   // DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
