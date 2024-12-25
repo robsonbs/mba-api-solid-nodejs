c5030233718927a3cc6e4d8f31ea6bb7e6f7c951
robson.souza <robson.souza@presidencia.gov.br>
2024-12-25 08:57:01 -0300

    feat (gyms): create new Gym

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,23 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'node:crypto'
+import { Decimal } from '@prisma/client/runtime/library'
 
 export class InMemoryGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    }
+    this.items.push(gym)
+    return gym
+  }
+
   public items: Gym[] = []
   async findById(id: string) {
     const gym = this.items.find((item) => item.id === id)
