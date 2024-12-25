44e197eb8038bfb66ca64b015911130a66267859
robson.souza <robson.souza@presidencia.gov.br>
2024-12-25 20:39:47 -0300

    feat: ger profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 
+import { profile } from '@/http/controllers/profile'
 import { usersRoutes } from './controllers/users'
 import { authenticateRoutes } from './controllers/authenticate'
 import { verifyJwt } from './middlewares/verify-jwt'
@@ -7,4 +8,7 @@ import { verifyJwt } from './middlewares/verify-jwt'
 export async function appRoutes(app: FastifyInstance) {
   app.register(usersRoutes, { prefix: 'users' })
   app.register(authenticateRoutes, { prefix: 'sessions' })
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
