565c58f435265cd1874b99399462b524729892b3
robson.souza <robson.souza@presidencia.gov.br>
2024-12-26 18:23:40 -0300

    test: gym features

---
+++ test/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,28 @@
+import request from 'supertest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+
+describe('Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to create a new gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const gymResponse = await request(app.server)
+      .post('/gyms')
+      .send({
+        title: 'JavaScript Gym',
+        description: null,
+        phone: null,
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(gymResponse.statusCode).toEqual(201)
+  }) // should be able to create a new gym (400ms)
+})
