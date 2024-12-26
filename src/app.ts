c33eb3971684097f96ac8935df900108be38d6a1
robson.souza <robson.souza@presidencia.gov.br>
2024-12-26 18:24:34 -0300

    build (refresh token): install packages

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
@@ -7,7 +8,16 @@ import { appRoutes } from './http/routes'
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(appRoutes)
 
